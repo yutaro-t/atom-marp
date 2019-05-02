125aaaa758698beb06911cfe913c5ebacaaf17a0
TotsukaYutaro <>
2019-05-02 23:31:41 +0900

    add some specs

--- spec/atom-marp-spec.js
+++ spec/atom-marp-spec.js
@@ -23,12 +23,11 @@ describe('AtomMarp', () => {
 
   })
 
-  const executeCommand = callback => {
+  const executeCommand = () => {
     runs(() => {
       atom.commands.dispatch(atom.workspace.getActiveTextEditor().getElement(), 'atom-marp:toggle')
     })
     waitsForPromise(() => activationPromise)
-    runs(callback)
   }
 
   const expectPreviewInSplitPane = () => {
@@ -48,14 +47,47 @@ describe('AtomMarp', () => {
   describe('when a preview has not been created for the file', function() {
     it('displays a markdown preview in a split pane', function() {
       waitsForPromise(() => atom.workspace.open('markdowns/test.md'))
-      executeCommand(() => {
-        expectPreviewInSplitPane()
-      })
+      executeCommand()
+      expectPreviewInSplitPane()
 
       runs(() => {
         const [editorPane] = atom.workspace.getCenter().getPanes()
         expect(editorPane.getItems()).toHaveLength(1)
         expect(editorPane.isActive()).toBe(true)
+      })
+    })
+  })
+
+  describe("when the editor's path does not exist", function () {
+    it('splits the current pane to the right with a markdown preview for the file', function () {
+      waitsForPromise(() => atom.workspace.open('new.markdown'))
+      executeCommand()
+      expectPreviewInSplitPane()
+    })
+  })
+
+  describe('when the editor does not have a path', function () {
+    it('splits the current pane to the right with a markdown preview for the file', function () {
+      waitsForPromise(() => atom.workspace.open(''))
+      executeCommand()
+      expectPreviewInSplitPane()
+    })
+  })
+
+  describe('when a preview has been created for the file', function () {
+
+    beforeEach(function () {
+      waitsForPromise(() => atom.workspace.open('markdowns/test.md'))
+      executeCommand()
+      expectPreviewInSplitPane()
+    })
+
+    it('closes the existing preview when toggle is triggered a second time on the editor', function () {
+      executeCommand()
+      runs(() => {
+        const [editorPane, previewPane] = atom.workspace.getCenter().getPanes()
+        expect(editorPane.isActive()).toBe(true)
+        expect(previewPane.getActiveItem()).toBeUndefined()
       })
     })
   })
